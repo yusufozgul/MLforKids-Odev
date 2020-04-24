9cfd1ca322705c9209c905558ab1b0a88a5b793e
Yusuf Özgül <yozgul@icloud.com>
2020-04-24 13:40:02 +0300

    Initial Commit

---
+++ MachineLearningForKidsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MachineLearningForKidsApp
+//
+//  Created by Yusuf Özgül on 24.04.2020.
+//  Copyright © 2020 Hızlı ve Buglı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
